141e4574514f94558cf73fa83b4498972b3d1b5a
Daniel Stotts <dstodev@gmail.com>
2020-09-30 11:08:23 -0700

    Adds sanity check regarding downcasting with static_cast

---
+++ personal/sandbox/cast_to_child.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct Parent
+{
+	virtual ~Parent() = default;
+	virtual void funcOne()
+	{
+		std::cout << "Parent: One" << std::endl;
+	}
+};
+
+struct Child : Parent
+{
+	void funcOne()
+	{
+		std::cout << "Child: One" << std::endl;
+	}
+	void funcTwo()
+	{
+		std::cout << "Child: Two" << std::endl;
+	}
+};
+
+int main()
+{
+	Parent * test = new Child;
+
+	test->funcOne();
+
+	static_cast<Child *>(test)->funcTwo();
+
+	delete test;
+
+	return 0;
+}

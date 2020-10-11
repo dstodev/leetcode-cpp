beaebf9531b9c44c70e41d656e92f58484c754a6
Daniel Stotts <dstodev@gmail.com>
2020-10-11 01:10:38 -0700

    How did this happen?

--- LeetCode/ReverseInteger/test/test_solution.cpp
+++ LeetCode/ReverseInteger/test/test_solution.cpp
@@ -8,77 +8,77 @@ using std::string;
 
 TEST(ReverseInteger_Solution, defaultConstructor)
 {
-    Solution uut;
+	Solution uut;
 }
 
 TEST(ReverseInteger_Solution, reverseGivenInputExample1)
 {
-    int input = 123;
-    int expected = 321;
+	int input = 123;
+	int expected = 321;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(ReverseInteger_Solution, reverseGivenInputExample2)
 {
-    int input = -123;
-    int expected = -321;
+	int input = -123;
+	int expected = -321;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(ReverseInteger_Solution, reverseGivenInputExample3)
 {
-    int input = 120;
-    int expected = 21;
+	int input = 120;
+	int expected = 21;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(ReverseInteger_Solution, reverseGivenInputExample4)
 {
-    int input = 0;
-    int expected = 0;
+	int input = 0;
+	int expected = 0;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(ReverseInteger_Solution, reverseZeroInMiddleOfNumber)
 {
-    int input = 901;
-    int expected = 109;
+	int input = 901;
+	int expected = 109;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }
 
 TEST(ReverseInteger_Solution, reverseMultiply10Overflow)
 {
-    int input = 1000000009;
-    int expected = 0;
+	int input = 1000000009;
+	int expected = 0;
 
-    Solution uut;
+	Solution uut;
 
-    int actual = uut.reverse(input);
+	int actual = uut.reverse(input);
 
-    ASSERT_EQ(expected, actual);
+	ASSERT_EQ(expected, actual);
 }

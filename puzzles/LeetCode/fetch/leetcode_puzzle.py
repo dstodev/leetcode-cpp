90762b5178725531c7c65ac6cc168e18abaf9518
Daniel Stotts <dstodev@gmail.com>
2021-04-19 15:44:38 -0700

    Working on LeetCode fetch script reformatting

--- puzzles/LeetCode/fetch/leetcode_puzzle.py
+++ puzzles/LeetCode/fetch/leetcode_puzzle.py
@@ -1,3 +1,5 @@
+import re
+
 from html_downloader import HTMLDownloader
 
 
@@ -27,13 +29,66 @@ class LeetCodePuzzle:
 
     def extract_puzzle_summary(self):
         selector = 'div[class*=question-content] > div > *'  # https://www.w3schools.com/css/css_combinators.asp
-        summary = self.html.find(selector)
-        for element in summary:
-            line = self.html_to_md(element)
-            self.summary.append(line)
+        selection = self.html.find(selector)
+        selection = [s.html for s in selection]
 
-    def html_to_md(self, element):
-        pass
+        summary = self.html_to_md(selection)
+        summary = self.reformat_puzzle_summary(summary)
+        summary = self.insert_placeholder_newlines(summary)
+        summary = self.remove_duplicate_whitespace_lines(summary)
+
+        self.summary = summary
+
+    def html_to_md(self, html):
+        if isinstance(html, str):
+            html = [html]
+            return_as_str = True
+        else:
+            return_as_str = False
+
+        md = []
+
+        for line in html:
+            line = re.sub(r'</?code>', '`', line)
+            line = re.sub(r'</?strong>', '**', line)
+            line = re.sub(r'</?em>', '*', line)
+            line = re.sub(r'</?pre>', '```', line)
+            line = re.sub(r'<p>', '', line)
+            line = re.sub(r'</p>', '\n', line)
+            line = re.sub(r'\xa0', ' ', line)
+            md.append(line)
+
+        if return_as_str:
+            md = md[0]
+
+        return md
+
+    def reformat_puzzle_summary(self, summary):
+        return summary
+
+    def insert_placeholder_newlines(self, lines):
+        expanded_lines = []
+
+        for line in lines:
+            tokens = line.split('\n')
+            expanded_lines.extend(tokens)
+
+        return expanded_lines
+
+    def remove_duplicate_whitespace_lines(self, lines):
+        sanitized_lines = []
+        whitespace_inserted = False
+
+        for line in lines:
+            if line.strip():
+                sanitized_lines.append(line)
+                whitespace_inserted = False
+            else:
+                if not whitespace_inserted:
+                    sanitized_lines.append('')
+                    whitespace_inserted = True
+
+        return sanitized_lines
 
     def extract_given_code(self):
         selector = '.CodeMirror'
--- puzzles/LeetCode/fetch/test_leetcode_puzzle.py
+++ puzzles/LeetCode/fetch/test_leetcode_puzzle.py
@@ -20,7 +20,7 @@ class TestLeetcodePuzzle(TestCase):
     valid_suffix = 'valid-puzzle-url-suffix'
 
     # The real deal!
-    #puzzle_suffix = 'two-sum'
+    # puzzle_suffix = 'two-sum'
     puzzle_url_suffix = 'add-two-numbers'
 
     @classmethod
@@ -83,9 +83,146 @@ class TestLeetcodePuzzle(TestCase):
         self.lcp.extract_puzzle_title()
         self.assertEqual('1. Two Sum', self.lcp.title)
 
+    def test_html_to_md_code_tag(self):
+        html = 'This is some <code>text</code>. It has <code>code snippets</code>.'
+        expected = 'This is some `text`. It has `code snippets`.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_strong_tag(self):
+        html = 'This is some <strong>text</strong>. It has <strong>bold parts</strong>.'
+        expected = 'This is some **text**. It has **bold parts**.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_em_tag(self):
+        html = 'This is some <em>text</em>. It has <em>italic parts</em>.'
+        expected = 'This is some *text*. It has *italic parts*.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_strong_and_em_tags(self):
+        html = 'This text has <strong>bold and <em>italic</em></strong> parts.'
+        expected = 'This text has **bold and *italic*** parts.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_pre_tag(self):
+        html = 'This is some <pre>text</pre>. It is <pre>preformatted</pre>.'
+        expected = 'This is some ```text```. It is ```preformatted```.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_p_tag(self):
+        html = '<p>This is some text.</p><p>It is written in paragraphs.</p>'
+        expected = 'This is some text.\nIt is written in paragraphs.\n'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_strips_hex_a0(self):
+        html = u'This is some text.\xa0It has a non-breaking space in it.'
+        expected = 'This is some text. It has a non-breaking space in it.'
+        actual = self.lcp.html_to_md(html)
+        self.assertEqual(expected, actual)
+
+    def test_html_to_md_accepts_lists(self):
+        html = [
+            'This is<pre>',
+            'some text</pre>.'
+        ]
+        expected = [
+            'This is```',
+            'some text```.'
+        ]
+        actual = self.lcp.html_to_md(html)
+        self.assertListEqual(expected, actual)
+
+    def test_reformat_puzzle_summary(self):
+        pass
+
+    def test_insert_placeholder_newlines(self):
+        lines = [
+            'These are\n',
+            'some\nlines',
+            'of text',
+            '\nfor testing.',
+        ]
+        expected = [
+            'These are',
+            '',
+            'some',
+            'lines',
+            'of text',
+            '',
+            'for testing.'
+        ]
+        actual = self.lcp.insert_placeholder_newlines(lines)
+        self.assertListEqual(expected, actual)
+
+    def test_remove_duplicate_whitespace_lines(self):
+        lines = [
+            'These are',
+            ' ',
+            '',
+            '\xa0',
+            'some',
+            '',
+            'lines',
+            ' ',
+            'of text',
+            u'\xa0',
+            'for testing.'
+        ]
+        expected = [
+            'These are',
+            '',
+            'some',
+            '',
+            'lines',
+            '',
+            'of text',
+            '',
+            'for testing.'
+        ]
+        actual = self.lcp.remove_duplicate_whitespace_lines(lines)
+        self.assertListEqual(expected, actual)
+
     def test_extract_puzzle_summary(self):
+        expected = [
+            'Given an array of integers `nums` and an integer `target`, return *indices of the two numbers such that they add up to `target`*.',
+            '',
+            'You may assume that each input would have ***exactly* one solution**, and you may not use the *same* element twice.',
+            '',
+            'You can return the answer in any order.',
+            '',
+            '**Example 1:**',
+            '```',
+            'Input: nums = [2,7,11,15], target = 9',
+            'Output: [0,1]',
+            'Output: Because nums[0] + nums[1] == 9, we return [0, 1].',
+            '```',
+            '',
+            '**Example 2:**',
+            '```',
+            'Input: nums = [3,2,4], target = 6',
+            'Output: [1,2]',
+            '```',
+            '',
+            '**Example 3:**',
+            '```',
+            'Input: nums = [3,3], target = 6',
+            'Output: [0,1]',
+            '```',
+            '',
+            '**Constraints:**',
+            '- `2 <= nums.length <= 103`',
+            '- `-109 <= nums[i] <= 109`',
+            '- `-109 <= target <= 109`',
+            '- *Only one valid answer exists.*'
+        ]
         self.lcp.get_puzzle_html()
         self.lcp.extract_puzzle_summary()
+        self.assertListEqual(expected, self.lcp.summary)
 
     def test_extract_given_code(self):
         expected = [

c0ea799d48c885d0bef8a5a7bd6ef2a52980d3ea
Daniel Stotts <dstodev@gmail.com>
2021-05-07 17:09:44 -0700

    Allows visitor to have parameters

--- personal/sandbox/static_iterator/main.cxx
+++ personal/sandbox/static_iterator/main.cxx
@@ -10,7 +10,7 @@ using std::size_t;
 enum class Values {
     Zero,
     One,
-    Two
+    Two,
 };
 
 template<Values value_T>
@@ -39,28 +39,41 @@ struct printer {
     }
 };
 
-template<typename Enum_T, size_t Enum_N, size_t i, template<Enum_T> class visitor_T>
-struct for_all_iterator {
-    for_all_iterator() {
-        visitor_T<static_cast<Enum_T>(i)>();
-        for_all_iterator<Enum_T, Enum_N, i + 1, visitor_T>();
+template<Values value_T>
+struct printer_with_arguments {
+    explicit printer_with_arguments(int value) {
+        cout << ToString<value_T>::name << ' ' << value << endl;
     }
 };
 
-template<typename Enum_T, size_t Enum_N, template<Enum_T> class visitor_T>
-struct for_all_iterator<Enum_T, Enum_N, Enum_N, visitor_T> {
-    // i == Enum_N; do nothing. Break recursion chain.
+template<typename Enum_T, size_t Enum_N, size_t i, template<Enum_T> class Visitor_T, typename... Args_T>
+struct for_all_iterator {
+    explicit for_all_iterator(Args_T...args) {
+        Visitor_T<static_cast<Enum_T>(i)>(args...);
+        for_all_iterator<Enum_T, Enum_N, i + 1, Visitor_T, Args_T...>(args...);
+    }
 };
 
-template<typename Enum_T, size_t Enum_N, template<Enum_T> class visitor_T>
+template<typename Enum_T, size_t Enum_N, template<Enum_T> class Visitor_T, typename... Args_T>
+struct for_all_iterator<Enum_T, Enum_N, Enum_N, Visitor_T, Args_T...> {
+    explicit for_all_iterator(Args_T...) {
+        /* i == Enum_N; do nothing. Break recursion chain. */
+    }
+};
+
+template<typename Enum_T, size_t Enum_N, template<Enum_T> class Visitor_T>
 struct for_all {
-    for_all() {
-        for_all_iterator<Enum_T, Enum_N, 0, visitor_T>();
+    template <typename... Args_T>
+    explicit for_all(Args_T...args) {
+        for_all_iterator<Enum_T, Enum_N, 0, Visitor_T, Args_T...>(args...);
     }
 };
 
 int main() {
-    for_all<Values, 3, printer>();
+    int a = 5;
+
+    for_all<Values, 3, printer>{};
+    for_all<Values, 3, printer_with_arguments>{a};
 
     return 0;
 }
